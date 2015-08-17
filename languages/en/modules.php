181a0fafbd77826a3cdbf296c6349da76adf9c05
Marion Baumgartner <marion.baumgartner@camptocamp.com>
2015-08-17 22:40:39 +0200

    change language part

--- languages/en/modules.php
+++ languages/en/modules.php
@@ -3,11 +3,11 @@
 /**
  * Back end modules
  */
-$GLOBALS['TL_LANG']['MOD']['cd_collection'] = array('CD Collection', 'Add and manage your favourite CDs.');
+$GLOBALS['TL_LANG']['MOD']['cd_collection'] = array('Ausschreibung', 'Eine Ausschreibung nue erstellen, löschen oder veränder');
 
 
 /**
  * Front end modules
  */
-$GLOBALS['TL_LANG']['FMD']['cd_list']   = array('CD List', 'Adds a list of CDs to the website.');
-$GLOBALS['TL_LANG']['FMD']['cd_reader'] = array('CD Reader', 'Adds a CD reader to the website.');
+$GLOBALS['TL_LANG']['FMD']['cd_list']   = array('Ausschreibung List', 'Adds a list of Ausschreibungen to the website.');
+$GLOBALS['TL_LANG']['FMD']['cd_reader'] = array('CD Reader', 'Adds a Ausschreibungs reader to the website.');
